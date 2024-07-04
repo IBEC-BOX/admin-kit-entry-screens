a33fb486263a428c88cfe753a6a99f5212369884
Anastas Mironov <ast@l80.ru>
2024-07-05 02:01:54 +0900

    fix: laravel-data version ^4.5

--- src/UI/API/DTO/EntryScreenDTO.php
+++ src/UI/API/DTO/EntryScreenDTO.php
@@ -4,12 +4,15 @@ namespace AdminKit\EntryScreens\UI\API\DTO;
 
 use AdminKit\EntryScreens\Models\EntryScreen;
 use Spatie\LaravelData\Attributes\MapName;
+use Spatie\LaravelData\Concerns\WithDeprecatedCollectionMethod;
 use Spatie\LaravelData\Data;
 use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 
 #[MapName(SnakeCaseMapper::class)]
 class EntryScreenDTO extends Data
 {
+    use WithDeprecatedCollectionMethod;
+
     public function __construct(
         public string $title,
         public string $background,
